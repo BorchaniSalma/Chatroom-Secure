8e90fd8f52c27bd45c9c8d02fe452ccd6aebe9d6
BorchaniSalma <sborcheni@ieee.org>
2023-01-19 15:54:05 +0100

    ldap

---
+++ cons.py
@@ -0,0 +1,19 @@
+# global variables :
+
+
+# Certification :
+dest_req = '/home/hime_chan/PycharmProjects/SecureChat/Certif/Client/requests'
+dest_key_cert = '/home/hime_chan/PycharmProjects/SecureChat/Certif/Client/private_keys'
+dest_client_cert = '/home/hime_chan/PycharmProjects/SecureChat/Certif/CA/clients_cert'
+dest_ca = "/home/hime_chan/PycharmProjects/SecureChat/Certif/CA/"
+
+# LDAP
+LDAP_HOST = "ldap://localhost:389"
+ADMIN_PWD = "admin"
+BASE_DN="dc=salma,dc=salma"
+ADMIN_DN = "cn=admin,dc=salma,dc=salma"
+USERS_DN="ou=security,dc=salma,dc=salma"
+
+# RSA
+PUB_KEYS = "/home/hime_chan/PycharmProjects/SecureChat/RSA/public_keys/"
+PRV_KEYS = "/home/hime_chan/PycharmProjects/SecureChat/RSA/private_keys/"
